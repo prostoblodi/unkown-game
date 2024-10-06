298f5d83f846e25b5aa669871408b9ce952c99ac
admin <dimatiktok.like8@gmail.com>
2024-10-06 15:00:33 +0300

    Пулья пофикшена

--- core/src/main/java/com/unkowngame/Bullet.java
+++ core/src/main/java/com/unkowngame/Bullet.java
@@ -10,6 +10,8 @@ public class Bullet {
     float x, y; // координаты пули
     float windowWidth = Gdx.graphics.getWidth();
     float windowHeight = Gdx.graphics.getHeight();
+    float leftBorder, bottomBorder;
+    float rightBorder, topBorder;
 
     boolean isActive = true; // активна ли пуля
 
@@ -33,11 +35,20 @@ public class Bullet {
         hitbox.x = x;
         hitbox.y = y;
 
+        leftBorder = (player.x - (windowWidth / 2));
+        bottomBorder = (player.y - (windowHeight / 2));
+
+        rightBorder = (player.x + (windowWidth / 2));
+        topBorder = (player.y + (windowHeight / 2));
+
         draw(shaperenderer);
 
-        if (x < -windowWidth - 1000 || x > windowWidth + 1000|| y < -windowHeight - 1000|| y > windowHeight + 1000) { // условие фиксить нада
+        if (x < leftBorder || x > rightBorder || y < bottomBorder || y > topBorder) { // условие фиксить нада
             isActive = false;
+            System.out.printf("|-> Bullet removed! \n L x: %f, y: %f -- leftBorder: %f, bottomBorder: %f -- rightBorder: %f, topBorder: %f \n",
+            x, y, leftBorder, bottomBorder, rightBorder, topBorder);
         }
+
 
     } // обновление пули
 
